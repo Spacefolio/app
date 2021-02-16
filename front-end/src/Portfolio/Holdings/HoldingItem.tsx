c5a5219318b8ae5cf5a6c485fad1b0562090f477
Tim <timothycrooker@gmail.com>
2021-02-16 13:23:19 -0500

    yes

--- front-end/src/Portfolio/Holdings/HoldingItem.tsx
+++ front-end/src/Portfolio/Holdings/HoldingItem.tsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { IPortfolioData, IPortfolioItem } from '../../../../types';
-import { FlexCard } from '../../_components';
+import React from "react";
+import { IPortfolioData, IPortfolioItem } from "../../../../types";
 
 interface HoldingItemProps {
-  portfolioItem: IPortfolioItem
+  portfolioItem: IPortfolioItem;
 }
 
-export const HoldingItem: React.FC<HoldingItemProps> = ({portfolioItem}) => {
-    return (<FlexCard children={}/><div style={{width: "100%", height: "100%"}}>
-      {portfolioItem.asset.name}  profit:{portfolioItem.profitTotal.all}
-    </div>);
-}
+export const HoldingItem: React.FC<HoldingItemProps> = ({ portfolioItem }) => {
+  return (
+    <div style={{ width: "100%", height: "100%" }}>
+      {portfolioItem.asset.name} profit{portfolioItem.profitTotal.all}
+    </div>
+  );
+};
