7de886273778d2e95f26da74dd5a7545757c58eb
Jake Knudson <jake41397@gmail.com>
2021-05-28 21:14:00 -0400

    +updates holdings in SyncExchangeAccountUseCase

--- back-end/src/core/use-cases/integration/exchangeAccount/syncExchangeAccount/SyncExchangeAccount.spec.ts
+++ back-end/src/core/use-cases/integration/exchangeAccount/syncExchangeAccount/SyncExchangeAccount.spec.ts
@@ -1,31 +1,61 @@
 import { ExchangeAccountNotFound, ExchangeAccountSyncFailed, ICreateExchangeAccountPayload, IExchangeAccountEntityGateway, SyncExchangeAccountRequest, SyncExchangeAccountUseCase } from "..";
 import makeFakeExchangeAccount from "../../../../../../__tests__/src/fixtures/ExchangeAccountFixture";
-import { getExchange } from "../../../../../../__tests__/src/fixtures/ExchangeInteractionFixture";
+import { getExchange, getExchangeThatFails } from "../../../../../../__tests__/src/fixtures/ExchangeInteractionFixture";
 import makeFakeUser from "../../../../../../__tests__/src/fixtures/UserFixture";
-import { ExchangeAccountInMemoryEntityGateway, UserInMemoryEntityGateway } from "../../../../../data";
+import { DigitalAssetInMemoryEntityGateway, ExchangeAccountInMemoryEntityGateway, UserInMemoryEntityGateway } from "../../../../../data";
+import DigitalAssetHistoryInMemoryEntityGateway from "../../../../../data/Integrations/DigitalAsset/DigitalAssetHistoryInMemoryEntityGateway";
 import { UseCaseError } from "../../../../definitions";
 import { Exchange, IExchangeAccount } from "../../../../entities";
 import { IUserEntityGateway, UserNotFound } from "../../../user";
+import { IDigitalAssetEntityGateway, IDigitalAssetHistoryEntityGateway } from "../../digitalAsset";
 
 describe('Get Exchange Account Use Case', () => {
   const userDatabase: IUserEntityGateway = new UserInMemoryEntityGateway();
   const exchangeAccountDatabase: IExchangeAccountEntityGateway = new ExchangeAccountInMemoryEntityGateway();
-  const syncExchangeAccountUseCase: SyncExchangeAccountUseCase = new SyncExchangeAccountUseCase(userDatabase, exchangeAccountDatabase, getExchange);
-  const syncFailsExchangeAccountUseCase: SyncExchangeAccountUseCase = new SyncExchangeAccountUseCase(userDatabase, exchangeAccountDatabase, getExchange);
+  const digitalAssetDatabase: IDigitalAssetEntityGateway = new DigitalAssetInMemoryEntityGateway();
+  const digitalAssetHistoryDatabase: IDigitalAssetHistoryEntityGateway = new DigitalAssetHistoryInMemoryEntityGateway();
+  const syncExchangeAccountUseCase: SyncExchangeAccountUseCase = new SyncExchangeAccountUseCase(userDatabase, exchangeAccountDatabase, digitalAssetDatabase, digitalAssetHistoryDatabase, getExchange);
+  const syncFailsExchangeAccountUseCase: SyncExchangeAccountUseCase = new SyncExchangeAccountUseCase(userDatabase, exchangeAccountDatabase, digitalAssetDatabase, digitalAssetHistoryDatabase, getExchangeThatFails);
   
   const fakeUser = makeFakeUser({});
+  const fakeUser2 = makeFakeUser({});
   const fakeExchangeAccount: IExchangeAccount = makeFakeExchangeAccount({});
+  const fakeExchangeAccount2: IExchangeAccount = makeFakeExchangeAccount({});
 
   const exchangeAccountRequest: ICreateExchangeAccountPayload = {
     accountId: fakeExchangeAccount.accountId,
     exchange: <Exchange>(fakeExchangeAccount.exchange.id),
     nickname: fakeExchangeAccount.nickname,
-    credentials: fakeExchangeAccount.credentials
+    credentials: fakeExchangeAccount.credentials,
+    transactions: fakeExchangeAccount.transactions,
+    holdings: fakeExchangeAccount.holdings,
+    orders: fakeExchangeAccount.orders,
+    openOrders: fakeExchangeAccount.openOrders,
+    dailyTimeslices: fakeExchangeAccount.dailyTimeslices,
+    hourlyTimeslices: fakeExchangeAccount.hourlyTimeslices
+  }
+
+  const exchangeAccountRequest2: ICreateExchangeAccountPayload = {
+    accountId: fakeExchangeAccount2.accountId,
+    exchange: <Exchange>(fakeExchangeAccount2.exchange.id),
+    nickname: fakeExchangeAccount2.nickname,
+    credentials: fakeExchangeAccount2.credentials,
+    transactions: fakeExchangeAccount2.transactions,
+    holdings: fakeExchangeAccount2.holdings,
+    orders: fakeExchangeAccount2.orders,
+    openOrders: fakeExchangeAccount2.openOrders,
+    dailyTimeslices: fakeExchangeAccount2.dailyTimeslices,
+    hourlyTimeslices: fakeExchangeAccount2.hourlyTimeslices
   }
 
   const request: SyncExchangeAccountRequest = {
     email: fakeUser.email,
     accountId: fakeExchangeAccount.accountId
+  }
+
+  const request2: SyncExchangeAccountRequest = {
+    email: fakeUser2.email,
+    accountId: fakeExchangeAccount2.accountId
   }
 
   beforeEach(async () => {
@@ -34,7 +64,6 @@ describe('Get Exchange Account Use Case', () => {
   });
 
   it('Syncs a valid exchange account', async () => {
-
     await userDatabase.createUser(fakeUser);
     const exchangeAccount = await exchangeAccountDatabase.createExchangeAccount(exchangeAccountRequest);
     await userDatabase.addExchangeAccountForUser(fakeUser.email, exchangeAccount);
@@ -43,7 +72,7 @@ describe('Get Exchange Account Use Case', () => {
 
     expect(response.isError).toBe(false);
     const account = response.getValue();
-    expect(account).toBe(exchangeAccount);
+    expect(account.holdings.length).toBe(2);
   });
 
   it('Returns user not found error', async () => {
@@ -81,11 +110,27 @@ describe('Get Exchange Account Use Case', () => {
 
   it('Returns sync failed on sync failure', async () => {
     await userDatabase.createUser(fakeUser);
-    await exchangeAccountDatabase.createExchangeAccount(exchangeAccountRequest);
+    const exchangeAccount = await exchangeAccountDatabase.createExchangeAccount(exchangeAccountRequest);
+    await userDatabase.addExchangeAccountForUser(fakeUser.email, exchangeAccount);
     const response = await syncFailsExchangeAccountUseCase.execute(request);
 
     expect(response.isError).toBe(true);
     const error = response.getError() as UseCaseError;
     expect(error).toBeInstanceOf(ExchangeAccountSyncFailed);
+  });
+
+  it('Updates holdings', async () => {
+    await userDatabase.createUser(fakeUser2);
+    const exchangeAccount2 = await exchangeAccountDatabase.createExchangeAccount(exchangeAccountRequest2);
+    await userDatabase.addExchangeAccountForUser(fakeUser2.email, exchangeAccount2);
+
+    const response = await syncExchangeAccountUseCase.execute(request2);
+
+    expect(response.isError).toBe(false);
+    const account = response.getValue();
+    expect(account.holdings[0].total.amount.bought).toBe(10);
+    expect(account.holdings[0].total.amount.sold).toBe(5);
+    expect(account.holdings[0].total.averageSellPrice.USD).toBe(15000);
+    expect(account.holdings[0].snapshots.length).toBe(3);
   });
 });
