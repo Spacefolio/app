689cde1146a4af11558745a63704a81cb869912d
Jake Knudson <jake41397@gmail.com>
2021-11-12 14:15:15 -0500

    +PM2 deploy config

---
+++ back-end/ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps : [{
+    name: "spacefolio-back-end",
+    script: './dist/app.js',
+    watch: './dist/app.js',
+    env: {
+      NODE_ENV: "development"
+    },
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'ubuntu',
+      host : 'ec2-54-159-188-168.compute-1.amazonaws.com',
+      key  : '~/.ssh/spacefolio-back-end.pem',
+      ref  : 'origin/main',
+      repo : 'git@github.com:Spacefolio/app.git',
+      path : '~/Spacefolio',
+      'pre-deploy-local': '',
+      'post-deploy' : 'cd ~/Spacefolio/back-end && npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
